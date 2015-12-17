d5b14a1188e06777d021fe0881022fd257837da7
saumitras <saumitra.srivastav7@gmail.com>
2015-12-17 18:11:54 +0530

    .

--- app/models/constants.scala
+++ app/models/constants.scala
@@ -1,6 +1,6 @@
 package object Constants {
   val DEFAULT_MONITOR_DB = "jdbc:h2:tcp://localhost/monitordb"
   val DEFAULT_EMAIL_DB = "jdbc:h2:tcp://localhost/emaildb"
-  val DEFAULT_LCP_DB = "jdbc:h2:tcp://localhost/gbmonitor"
-  val DEFAULT_ZK_HOST = "localhost:2181"
+  val DEFAULT_LCP_DB = ""// "jdbc:h2:tcp://localhost/gbmonitor"
+  val DEFAULT_ZK_HOST = "" //localhost:2181"
 }
