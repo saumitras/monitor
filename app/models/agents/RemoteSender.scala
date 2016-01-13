f4066f7d0bff2f710ded9380bbf642c342ac7dbd
saumitras <saumitra.srivastav7@gmail.com>
2016-01-13 13:52:29 +0530

    .

--- app/models/agents/RemoteSender.scala
+++ app/models/agents/RemoteSender.scala
@@ -8,7 +8,7 @@ class RemoteSender extends Actor {
   override def receive: Receive = {
     case ActivateAgent(eventId:String) =>
       val remote = context.actorSelection("akka.tcp://GBMonitor@127.0.0.1:7390/user/RemoteListener")
-      remote ! s"$eventId,GET_LCP_LOG,GET_LCP_HEAPDUMP"
+      remote ! s"$eventId,GET_LCP_HEAPDUMP|GET_LCP_LOG"
 
   }
 }
